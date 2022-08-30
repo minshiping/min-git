dff481a57d41fc15c6b927b56c63b1d28d73e8ba
minshiping <109787958+minshiping@users.noreply.github.com>
2022-08-30 19:14:13 +0800

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,37 @@
+Python 3.11.0a2 (tags/v3.11.0a2:e2b4e4b, Nov  5 2021, 19:50:05) [MSC v.1929 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print 'hello,word'
+SyntaxError: Missing parentheses in call to 'print'. Did you mean print(...)?
+yes
+Traceback (most recent call last):
+  File "<pyshell#1>", line 1, in <module>
+    yes
+NameError: name 'yes' is not defined
+
+
+ print 'hello,world'
+ 
+SyntaxError: unexpected indent
+
+
+
+print 'hello,world'
+SyntaxError: Missing parentheses in call to 'print'. Did you mean print(...)?
+print（'hello world'）
+SyntaxError: invalid character '（' (U+FF08)
+Print("Hello World!")
+Traceback (most recent call last):
+  File "<pyshell#10>", line 1, in <module>
+    Print("Hello World!")
+NameError: name 'Print' is not defined. Did you mean: 'print'?
+Print("Hello World")
+Traceback (most recent call last):
+  File "<pyshell#11>", line 1, in <module>
+    Print("Hello World")
+NameError: name 'Print' is not defined. Did you mean: 'print'?
+print（'hello world'）
+SyntaxError: invalid character '（' (U+FF08)
+print（'hello world'(U+FF08)）
+SyntaxError: invalid character '（' (U+FF08)
+print('hello world')
+hello world
